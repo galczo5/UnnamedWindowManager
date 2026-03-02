97f8b298fa1686b88f4508186268d8800d798c53
galczo5 <kamil.galek@gmail.com>
2026-03-02 17:30:16 +0100

    Initial Commit

---
+++ UnnamedWindowManager/UnnamedWindowManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnnamedWindowManagerApp.swift
+//  UnnamedWindowManager
+//
+//  Created by Kamil on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct UnnamedWindowManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
